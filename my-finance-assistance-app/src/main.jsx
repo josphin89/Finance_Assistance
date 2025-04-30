5e1d3fcf3f68f6ec4d403f21615b8a8cf6becdfe
josphin89 <josejosphin89@gmail.com>
2025-04-30 16:33:23 +0530

    Aded slash

--- my-finance-assistance-app/src/main.jsx
+++ my-finance-assistance-app/src/main.jsx
@@ -28,6 +28,10 @@ const router = createBrowserRouter([
     element: (<Login/>),
   },
   {
+    path: "/",
+    element: (<Login/>),
+  },
+  {
     path: "/register",
     element:(<Register/>),
 
